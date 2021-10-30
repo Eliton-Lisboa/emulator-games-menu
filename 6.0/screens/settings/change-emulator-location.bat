b3fa9d42060fd6126d476b8c5fa3faed243ce267
Eliton-Lisboa <elitonlisboa303@gmail.com>
2021-10-30 15:33:54 -0300

    add share locations

--- 6.0/screens/settings/change-emulator-location.bat
+++ 6.0/screens/settings/change-emulator-location.bat
@@ -2,11 +2,31 @@
 setlocal enabledelayedexpansion
 
 title !window-title! - Change emulator location
-mode !window-size-width!,13
+mode !window-size-width!,14
 color !window-color!
 
+set "menu="
+set "menu-show="
+set "item="
+
+set "result="
 set "new-value="
 set error-level=0
+
+:ini (
+  set "menu="
+  set "menu-show="
+
+  call database\get\shared-locations "emulator", menu, menu-show
+
+  for %%x in ("" "Write a new location" "Back") do (
+    call lib\center-text %%x, result
+    set menu=!menu! %%x
+    set menu-show=!menu-show! "!result!"
+  )
+
+  goto :home
+)
 
 :home (
   cls
@@ -14,25 +34,39 @@ set error-level=0
   call components\draw-title "Console Games Menu"
   call lib\draw-center-text "{&1&5}!user-emulator-location!", 1
   echo.
-  call lib\draw-center-text "Type {&1&4}'back'{&0} to go back", 1
+  call lib\draw-center-text "Type '{&1&4}back{&0}' to go back", 1
   echo.
   echo.
 
-  :home-location (
+  cmdmenusel f880 !menu-show!
+
+  call lib\get-array-vector menu, !errorlevel!, result
+  set result=!result:~1,-1!
+
+  :home-type
+  if "!result!" == "Back" (
+    exit
+  ) else if "!result!" == "Write a new location" (
     call components\draw-input-errorlevel "Type new emulator location", !error-level!
     set /p "new-value="
 
     if "!new-value!" == "back" exit
-    if exist "!new-value!" (
-      call database\update\emulator-location "!user-name!", "!new-value!"
+    if exist "!new-value!\*" (
+      call database\update\locations "!user-name!", "emulator", "!new-value!"
       set user-emulator-location=!new-value!
 
       exit
     ) else (
       set error-level=3
-      goto :home-location
+      goto :home-type
     )
 
+  ) else if "!result!" neq "" (
+    call database\update\locations "!user-name!", "emulator", "!result!"
+    set user-emulator-location=!result!
+
+    exit
   )
 
+  goto :home
 )
--- 6.0/screens/settings/change-roms-location.bat
+++ 6.0/screens/settings/change-roms-location.bat
@@ -2,11 +2,31 @@
 setlocal enabledelayedexpansion
 
 title !window-title! - Change roms location
-mode !window-size-width!,13
+mode !window-size-width!,14
 color !window-color!
 
+set "menu="
+set "menu-show="
+set "item="
+
+set "result="
 set "new-value="
 set error-level=0
+
+:ini (
+  set "menu="
+  set "menu-show="
+
+  call database\get\shared-locations "roms", menu, menu-show
+
+  for %%x in ("" "Write a new location" "Back") do (
+    call lib\center-text %%x, result
+    set menu=!menu! %%x
+    set menu-show=!menu-show! "!result!"
+  )
+
+  goto :home
+)
 
 :home (
   cls
@@ -14,25 +34,39 @@ set error-level=0
   call components\draw-title "Console Games Menu"
   call lib\draw-center-text "{&1&5}!user-roms-location!", 1
   echo.
-  call lib\draw-center-text "Type {&1&4}'back'{&0} to go back", 1
+  call lib\draw-center-text "Type '{&1&4}back{&0}' to go back", 1
   echo.
   echo.
 
-  :home-location (
+  cmdmenusel f880 !menu-show!
+
+  call lib\get-array-vector menu, !errorlevel!, result
+  set result=!result:~1,-1!
+
+  :home-type
+  if "!result!" == "Back" (
+    exit
+  ) else if "!result!" == "Write a new location" (
     call components\draw-input-errorlevel "Type new roms location", !error-level!
     set /p "new-value="
 
     if "!new-value!" == "back" exit
     if exist "!new-value!\*" (
-      call database\update\roms-location "!user-name!", "!new-value!"
+      call database\update\locations "!user-name!", "roms", "!new-value!"
       set user-roms-location=!new-value!
 
       exit
     ) else (
       set error-level=3
-      goto :home-location
+      goto :home-type
     )
 
+  ) else if "!result!" neq "" (
+    call database\update\locations "!user-name!", "roms", "!result!"
+    set user-roms-location=!result!
+
+    exit
   )
 
+  goto :home
 )
