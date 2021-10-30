7d0753d50a36d017631f1e9dd1bf85b2b64f4a6e
Eliton-Lisboa <elitonlisboa303@gmail.com>
2021-10-30 20:51:53 -0300

    update .bat to .cmd if they don't have UI

--- 6.0/database/update/questions.bat
+++
@@ -1,9 +0,0 @@
-setlocal enabledelayedexpansion & : [name, question1, answer1, question2, answer2, question3, answer3]
-  echo %~2=%~3> "database\users\%~1\recovery-questions.txt"
-  echo %~4=%~5>> "database\users\%~1\recovery-questions.txt"
-  echo %~6=%~7>> "database\users\%~1\recovery-questions.txt"
-
-(
-  endlocal
-  exit /b 0
-)
