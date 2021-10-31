41b8f5d92e16872cbb276b0200d7a2e5cb5fb60d
Eliton-Lisboa <elitonlisboa303@gmail.com>
2021-10-30 22:33:26 -0300

    remove questions show with cecho, now is show with \/p and update add user route

--- 6.0/screens/register.bat
+++ 6.0/screens/register.bat
@@ -90,30 +90,24 @@ set "if-or=n"
     echo.
 
     :home-question-1 (
-      cecho  Question 1: 
-      set /p "new-user-questions-1="
-      cecho  Answer 1: 
-      set /p "new-answers-1="
+      set /p "new-user-questions-1=Question 1: "
+      set /p "new-answers-1=Answer 1: "
 
       if "!new-user-questions-1!" == "" goto :home-questions-1
       if "!new-user-questions-1!" == "back" exit
     )
 
     :home-question-2 (
-      cecho  Question 2: 
-      set /p "new-user-questions-2="
-      cecho  Answer 2: 
-      set /p "new-answers-2="
+      set /p "new-user-questions-2=Question 2: "
+      set /p "new-answers-2=Answer 2: "
 
       if "!new-user-questions-2!" == "" goto :home-questions-2
       if "!new-user-questions-2!" == "back" exit
     )
 
     :home-question-3 (
-      cecho  Question 3: 
-      set /p "new-user-questions-3="
-      cecho  Answer 3: 
-      set /p "new-answers-3="
+      set /p "new-user-questions-3=Question 3: "
+      set /p "new-answers-3=Answer 3: "
 
       if "!new-user-questions-3!" == "" goto :home-questions-3
       if "!new-user-questions-3!" == "back" exit
@@ -121,7 +115,7 @@ set "if-or=n"
 
   )
 
-  call database\add "!new-user-name!", "!new-user-pass!"
+  call database\add\user "!new-user-name!", "!new-user-pass!"
   call database\update\recovery-questions "!new-user-name!", "!new-user-questions-1!:!new-answers-1!;!new-user-questions-2!:!new-answers-2!;!new-user-questions-3!:!new-answers-3!"
 
   set "user-name=!new-user-name!"
