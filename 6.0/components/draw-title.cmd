b372bef891f343bb7938f46049504fdf487b85cc
pandasoli <sadpandasoli@gmail.com>
2022-06-14 01:03:17 -0300

    version 1.0

--- 6.0/components/draw-title.cmd
+++
@@ -1,7 +0,0 @@
-setlocal enabledelayedexpansion & : [text]
-	call !window-libs-path!\draw-center-text "{&1&3}-{&1&4}-{&1&5}-{&1&2} %~1 {&1&3}-{&1&4}-{&1&5}-", 4
-
-(
-	endlocal
-	exit /b 0
-)
--- 6.0/database/update/share-locations.cmd
+++
@@ -1,9 +0,0 @@
-setlocal enabledelayedexpansion & : [name, location name, new value]
-  if exist "database\users\%~1\*" (
-    echo %~3> "database\users\%~1\share-%~2-location.txt"
-  )
-
-(
-  endlocal
-  exit /b 0
-)
